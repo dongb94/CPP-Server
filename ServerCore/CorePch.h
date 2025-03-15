4870570c0b79fb391319df78508a813072000cd1
MagicDog <dongb94@gmail.com>
2025-03-15 14:12:14 +0900

    Add Project

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <set>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
---
+++ ServerCore/pch.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN // 거의 사용되지 않는 내용을 window 헤더에서 제거합니다.
+
+#include "CorePch.h"
+
